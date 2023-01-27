bb31739eb170db2a8de34c0a1a74bef013b581b6
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-27 09:42:05 -0500

    formulario terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sincrono28';
+export class AppComponent implements OnInit{
+  title = 'Formulario para test';
+  mensaje!:string
+  public form!:FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.form=this.fb.group({
+      user:['', [Validators.required, Validators.minLength(4)]],
+      pass:['', [Validators.required, Validators.minLength(4)]]
+    })
+  }
+
+  enviar():void{
+    console.log(this.form.value)
+    this.mensaje='Formulario enviado'
+  }
 }
